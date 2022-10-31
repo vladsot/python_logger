3857e1429999c50efbfc9875b292ca6d679f77ac
Vlad Sotnikov <vladsot@outlook.com>
2022-10-31 14:22:37 -0700

    periodic commit

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 import logging
 import logging.handlers
 from logging.handlers import SysLogHandler
+
+logger = logging.getLogger('myLogger')
+
+logger.setLevel(logging.DEBUG)
+
+
+
+handler = SysLogHandler(facility=SysLogHandler.LOG_DAEMON, address='/dev/log')
+logger.addHandler(handler)
+
+logger.debug('Fooo!')
+
